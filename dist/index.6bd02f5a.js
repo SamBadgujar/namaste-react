72cba30ece90cb33d7d0f2b0ffd3b7951319c559
samadhan <samadhanvishnu7757@gmail.com>
2025-03-14 02:30:34 +0530

    changes

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,15 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, React.createElement("h1", {
+    id: "heading"
+}, "Hello World From React!!!!!!")));
+console.log(parent); // Logs React element object
+const rootElement = document.getElementById("root");
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(parent); // Render the correct element
+} else console.error("Root element not found!");
+
+//# sourceMappingURL=index.6bd02f5a.js.map
